817265edc2a91941784f0579523e7923871cd667
Racheal W <williamsracheal@hotmail.com>
2018-01-10 15:19:28 +0000

    refactored userEmail function

--- web_scraper/scraper.js
+++ web_scraper/scraper.js
@@ -1,16 +1,18 @@
 // User inputs email
-var EMAIL_REGEX = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
- 
+// var EMAIL_REGEX = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+var EMAIL_REGEX = "tim@canddi.com";
+
 function userEmail() {
   var email = prompt ("Please enter your email address");
-  if((email).toLowerCase() == EMAIL_REGEX) 
-    return true; 
-  else {
-    alert("You have entered an invalid email.");   
-    return false;
+
+  if(email.toLowerCase() == EMAIL_REGEX) 
+   return true;
+  {
+   alert("You have entered an invalid email.");
+   return false;
   }
 }
-
+ 
 // Create request to scrape website
 
 // Use Cheerio to load homepage
