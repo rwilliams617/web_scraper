b7e66d8ad39f980ecf4341fbf9818f6e10fff70f
Racheal W <williamsracheal@hotmail.com>
2018-01-10 14:38:42 +0000

    userEmail function, works in browser

--- web_scraper/scraper.js
+++ web_scraper/scraper.js
@@ -1,4 +1,15 @@
-// User inputs email 
+// User inputs email
+var EMAIL_REGEX = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+ 
+function userEmail() {
+  var email = prompt ("Please enter your email address");
+  if((email).toLowerCase() == EMAIL_REGEX) 
+    return true; 
+  else {
+    alert("You have entered an invalid email.");   
+    return false;
+  }
+}
 
 // Create request to scrape website
 
