7565e6263f59c235717369bea24c1e2a14688dfb
Racheal W <williamsracheal@hotmail.com>
2018-01-10 13:08:14 +0000

    initial commit

---
+++ web_scraper/scraper.js
@@ -0,0 +1,9 @@
+// User inputs email 
+
+// Create request to scrape website
+
+// Use Cheerio to load homepage
+
+// Use Knwl to parse the data to retrieve company info, store in array
+
+// Print data to screen
