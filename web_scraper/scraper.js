9dde3e46ae73dee75e3265ef1f90ef31cec4b0d8
Racheal W <williamsracheal@hotmail.com>
2018-01-14 22:34:15 +0000

    created displayContent function, to access/print json object values to screen

--- web_scraper/scraper.js
+++ web_scraper/scraper.js
@@ -18,7 +18,7 @@ function domainName() {
   var domain = email.toString().replace(/.*@/, ""); 
   return domain;
 }
- 
+
 // Create request to scrape website
 // Use Cheerio to load homepage
 
@@ -36,19 +36,42 @@ request(website, function (error, response, body) {
   if(response.statusCode === 200) {
     var cheerio = require('cheerio');
     var $ = cheerio.load(body);
+    // console.log(body);
 
     function extractContent() {
       var data = $('script[type="application/ld+json"]').html();
       var json = JSON.parse(data);
-      var text = JSON.stringify(json);
+      // var text = JSON.stringify(json); took this out as it didn't work with json dot notation method
 
-      return(text);
+      return(json);
     }
-    console.log(extractContent());
+    // console.log(text);
+    // console.log(extractContent()); 
 
-//to-do create function where we take string objects in extractContent() and trim/split into one string to be parsed using Knwl.js
+//Create function to access/print json object values to screen
+    function displayContent() {
+      var jsonObj = extractContent()
+      var objName = "Name: " + jsonObj.name + ", " + jsonObj.legalName
+      var objDate = "Founding Date: " + jsonObj.foundingDate
+      var objAddress = "Address: " +
+      jsonObj.address.streetAddress + ", " 
+      + jsonObj.address.addressRegion + ", " 
+      + jsonObj.address.postalCode
+      var objPhone = "Phone: " + jsonObj.contactPoint.telephone
+      var objEmail = "Email: " + jsonObj.contactPoint.email
+      var objUrl = "Website: " + jsonObj.url
+      var objSM = "Social Media: " + jsonObj.sameAs
 
 
+      console.log(objName) 
+      console.log(objDate)
+      console.log(objAddress)
+      console.log(objPhone)
+      console.log(objEmail)
+      console.log(objUrl)
+      console.log(objSM)
+    }
+    console.log(displayContent());
   }
 
 });
