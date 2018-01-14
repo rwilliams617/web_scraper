faa9b3259e8549a7eff5f37c342116cc9dacec7c
Racheal W <williamsracheal@hotmail.com>
2018-01-14 00:50:04 +0000

    extractContent function, using JSON to parse data from 'application/ld+json' script tag, refactored code

--- web_scraper/scraper.js
+++ web_scraper/scraper.js
@@ -33,49 +33,19 @@ request(website, function (error, response, body) {
 
   console.log('Status Code:', response && response.statusCode); // Print the response status code if a response was received
 
-// Use Knwl to parse the data to retrieve company info, store in array
   if(response.statusCode === 200) {
     var cheerio = require('cheerio');
     var $ = cheerio.load(body);
-    console.log($('footer').text());
 
-    $('a').each(function() {
-      var text = $(this).text();
-      var link = $(this).attr('href');
+    function extractContent() {
+      var data = $('script[type="application/ld+json"]').html();
+      var json = JSON.parse(data);
+      var text = JSON.stringify(json);
 
-        if(link === "tel:+44 161 414 1080" && "^(\(?\+?[0-9]*\)?)?[0-9_\- \(\)]*$") {
-          var phone = "^\+?[1-9]\d{1,14}$"; //E.164 general format for international telephone numbers regex
-          console.log(phone);
-          return phone;
+      return(text);
+    }
+    console.log(extractContent());
 
-      function rgCompanyPhones() {
-        var Knwl = require('knwl.js')  
-        var knwlInstance = new Knwl('english');
+  }
 
-        knwlInstance.register('phones', require('knwl.js/default_plugins/phones.js'));
-
-        knwlInstance.init(phone); 
-        var phones = knwlInstance.get('phones');
-
-        return(phones);
-      }
-      console.log(rgCompanyPhones()); 
-      }
-
-      function rgCompanyLinks() {
-        var Knwl = require('knwl.js')  
-        var knwlInstance = new Knwl('english');
-
-        knwlInstance.register('links', require('knwl.js/default_plugins/links.js'));
-
-        knwlInstance.init(link);
-        var links = knwlInstance.get('links');
-
-        return(links);
-      }
-// Print data to screen
-      console.log(rgCompanyLinks());
-  
-    });
-  };
 });
